e3652f39d23566f79fcfd60f9ce1c8e05ee5e2c3
CristianJA2009 <cristian.a2009@aluno.ifsc.edu.br>
2026-06-24 16:33:49 -0300

    style: pg carrinho front-end

--- ProjetoFinal_IFSC_Info3/obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
+++ ProjetoFinal_IFSC_Info3/obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4705a7ab9d39397f09a47cee34f65ac992fb6977")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+61eb71773721833a15be3e9e6c4cbd2717da660a")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
