a8edcb29d0323d12187d1e19cc58d329fbbbb145
matheus-mp <matheusmachadopires777@gmail.com>
2026-06-25 16:24:54 -0300

    back-end: database entities

--- Models/Categoria.cs
+++
@@ -1,9 +0,0 @@
-namespace ProjetoFinal_IFSC_info3
-{
-    public class Categoria
-    {
-        public int id { get; set; }
-        public string nome { get; set; }
-        public ICollection<Jogo> Jogos { get; set; } = new List<Jogo>();
-    }
-}
--- obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
+++ obj/Debug/net10.0/MeuProjeto.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9641be563820d90307620cd5c887a7d29491c127")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f6f417fce652a2c0d5c66c8e57053542a5537a7c")]
 [assembly: System.Reflection.AssemblyProductAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyTitleAttribute("MeuProjeto")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
