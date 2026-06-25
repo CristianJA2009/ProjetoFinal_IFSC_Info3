20ac07db61816abd2c44e0be92f22443cce34de2
CristianJA2009 <cristian.a2009@aluno.ifsc.edu.br>
2026-06-25 14:04:10 -0300

    style: subtitle fix

---
+++ Models/Usuario.cs
@@ -0,0 +1,14 @@
+namespace ProjetoFinal_IFSC_info3
+{
+    public class Usuario
+    {
+        public int id { get; set; }
+        public string nome { get; set; }
+        public string email { get; set; }
+        public string senha { get; set; }
+        public string foto { get; set; }
+        public int pontos { get; set; }
+        public DateTime criado_em { get; set; }
+        public string tipo { get; set; }
+    }
+}
