9ee19334d91fe6b302a4a287d75b502c66b2bbd0
matheus-mp <matheusmachadopires777@gmail.com>
2026-06-26 16:16:54 -0300

    backend: database models

---
+++ Models/LegendsStoreContext.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MeuProjeto.Models
+{
+    public class LegendsStoreContext : DbContext
+    {
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
---
+++ Models/Usuario.cs
@@ -0,0 +1,8 @@
+﻿namespace MeuProjeto.Models
+{
+    public class Usuario
+    {
+        public int Id { get; set; }
+        public string nome { get; set; }
+    }
+}
